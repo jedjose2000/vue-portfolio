c0a1b9ae3be74452ca316c68686c77ea1fdc0322
jedjose2000 <jedjose2000@gmail.com>
2024-02-27 12:06:33 +0800

    testing for api

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-// axios.defaults.baseURL = "http://laravel-app-env.eba-ikq93um4.ap-southeast-2.elasticbeanstalk.com/api/";
+axios.defaults.baseURL = "http://laravel-app-env.eba-ikq93um4.ap-southeast-2.elasticbeanstalk.com/api/";
 
-axios.defaults.baseURL = "http://localhost:8000/api/";
+// axios.defaults.baseURL = "http://localhost:8000/api/";
